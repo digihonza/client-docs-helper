fbe51edbf756f098ce46dafdead00945c4955c9b
Jan Piskacek <piskacekjan@gmail.com>
2024-02-08 11:04:07 +0100

    Implement app configuration read and write

---
+++ ClientDocsHelper/AppConfiguration/AppConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace ClientDocsHelper.AppConfiguration
+{
+    internal class AppConfiguration
+    {
+        public string? TemplateFolderPath { get; set; }
+        public string? ClientsRootPath { get; set; }
+
+        public bool HasTemplateFolderPath => !string.IsNullOrWhiteSpace(TemplateFolderPath);
+        public bool HasClientsRootPath => !string.IsNullOrWhiteSpace(ClientsRootPath);
+
+        public bool IsValid => HasTemplateFolderPath && HasClientsRootPath;
+    }
+}
---
+++ ClientDocsHelper/AppConfiguration/AppConfigurationReader.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+
+namespace ClientDocsHelper.AppConfiguration
+{
+    internal class AppConfigurationReader
+    {
+        private readonly string filePath;
+
+        public AppConfigurationReader(string filePath)
+        {
+            this.filePath = filePath ?? throw new ArgumentException("File path not provided");
+        }
+
+        public async Task<AppConfiguration?> ReadConfiguration()
+        {
+            var configJson = await GerConfigJsonFromFile();
+            return string.IsNullOrWhiteSpace(configJson) ? null : GetAppConfigurationFromJson(configJson);
+        }
+
+        private async Task<string?> GerConfigJsonFromFile()
+        {
+            string? configJson = null;
+            try
+            {
+                configJson = await File.ReadAllTextAsync(filePath);
+            }
+            catch (FileNotFoundException)
+            {
+                configJson = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Stala se chyba při čtení konfiguračního souboru. ({ex.Message})");
+            }
+
+            return configJson;
+        }
+
+        private AppConfiguration? GetAppConfigurationFromJson(string json)
+        {
+            AppConfiguration? appConfiguration = null;
+            try
+            {
+                appConfiguration = JsonSerializer.Deserialize<AppConfiguration>(json);
+            } catch (JsonException)
+            {
+                Console.WriteLine($"Stala se chyba při čtení konfiguračního souboru. Obsah konfigurační soubor má špatný formát, musí být validní JSON.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Stala se chyba při čtení konfiguračního souboru. ({ex.Message})");
+            }
+            return appConfiguration;
+        }
+    }
+}
